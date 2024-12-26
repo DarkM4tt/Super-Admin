e0bbd1c858230392b4bacbbd4bf3db16cff358a4
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2024-12-26 16:58:34 +0530

    Added header and partners page

--- src/components/common/HomeHeader.jsx
+++ src/components/common/HomeHeader.jsx
@@ -1,51 +1,17 @@
 import { useNavigate } from "react-router-dom";
-import avatarUser from "../../assets/avatarUser.svg";
 import boldCyan from "../../assets/boldCyan.svg";
-import { Badge, IconButton } from "@mui/material";
-import NotificationsIcon from "@mui/icons-material/Notifications";
+import { IconButton } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
-import { styled } from "@mui/material/styles";
-import { useState } from "react";
-import Notification from "./Notification";
-import MenuIcon from '@mui/icons-material/Menu';
-
-const StyledBadge = styled(Badge)(({ theme }) => ({
-  "& .MuiBadge-badge": {
-    backgroundColor: "#D20B0B",
-    color: "#D20B0B",
-    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    "&::after": {
-      position: "absolute",
-      width: "100%",
-      height: "100%",
-      borderRadius: "50%",
-      animation: "ripple 1.2s infinite ease-in-out",
-      border: "1px solid currentColor",
-      content: '""',
-    },
-  },
-  "@keyframes ripple": {
-    "0%": {
-      transform: "scale(.8)",
-      opacity: 1,
-    },
-    "100%": {
-      transform: "scale(2.4)",
-      opacity: 0,
-    },
-  },
-}));
+import MenuIcon from "@mui/icons-material/Menu";
 
 // eslint-disable-next-line react/prop-types
-const HomeHeader = ({ notification,showsidebar, setshowsidebar }) => {
+const HomeHeader = ({ showsidebar, setshowsidebar }) => {
   const navigate = useNavigate();
-  const [shownotification, setshownotification] = useState(false);
 
   const handleClick = () => {
     navigate("/home");
   };
 
-  // Logout function to clear org_id from local storage and navigate to login page
   const handleLogout = () => {
     localStorage.removeItem("org_id");
     localStorage.removeItem("auth_token");
@@ -54,7 +20,7 @@ const HomeHeader = ({ notification,showsidebar, setshowsidebar }) => {
   };
 
   return (
-    <div className="flex items-center justify-between border-b-[1px] border-lightGray">
+    <div className="flex items-center justify-between shadow-md">
       <div className="bg-themeBlue w-[42.5%]  sm:w-1/5 py-4 flex items-center max-w-[280px] gap-4 justify-center md:justify-start flex-col md:flex-row ">
         <img
           src={boldCyan}
@@ -62,40 +28,35 @@ const HomeHeader = ({ notification,showsidebar, setshowsidebar }) => {
           className="w-20 cursor-pointer md:ml-10"
           onClick={handleClick}
         />
-      <div className=" block md:hidden text-white" onClick={()=>{setshowsidebar(!showsidebar)}}><MenuIcon fontSize="large"/></div>
-        
-      </div>
-      <div className="flex items-center space-x-4 mr-10">
-        <p className="font-semibold text-lg">Mr. Jorge</p>
-        <img src={avatarUser} alt="bold" className="w-10 cursor-pointer" />
         <div
-          className="bg-[#EEEEEE] rounded-lg p-[6px] relative"
-          onClick={() => setshownotification(!shownotification)}
+          className=" block md:hidden text-white"
+          onClick={() => {
+            setshowsidebar(!showsidebar);
+          }}
         >
-          {notification ? (
-            <StyledBadge
-              overlap="circular"
-              anchorOrigin={{ vertical: "top", horizontal: "right" }}
-              variant="dot"
-            >
-              <NotificationsIcon sx={{ fontSize: "20px" }} />
-            </StyledBadge>
-          ) : (
-            <NotificationsIcon sx={{ fontSize: "20px" }} />
-          )}
-          {shownotification && (
-            <div className="absolute top-24 right-0 w-[434px] max-w-screen-sm z-50">
-              <Notification
-                message={notification ? notification : "No Notifications"}
-                date={"July 24, 2024"}
-              />
-            </div>
-          )}
+          <MenuIcon fontSize="large" />
         </div>
-        {/* Logout button */}
-        <IconButton onClick={handleLogout}>
-          <LogoutIcon />
-        </IconButton>
+      </div>
+      <div className="flex-1 flex flex-row justify-between items-center px-10">
+        <p className="font-redhat text-2xl font-semibold">Owner Access</p>
+        <div className="flex flex-row gap-10">
+          <div
+            className="flex flex-row items-center cursor-pointer hover:bg-gray-100 p-2 rounded-md group"
+            onClick={handleLogout}
+          >
+            <IconButton
+              sx={{
+                color: "red",
+                "&:hover": { backgroundColor: "transparent" }, // Remove default hover effect
+              }}
+            >
+              <LogoutIcon />
+            </IconButton>
+            <p className="font-redhat text-2xl font-semibold ml-2 group-hover:text-red-600">
+              Log out
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
